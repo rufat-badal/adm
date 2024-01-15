53b3ce8722b96addb9861d108ee2292371cf46ef
Rufat Badal <19675055+rufat-badal@users.noreply.github.com>
2024-01-15 18:05:15 +0100

    Create a linked list from a slice

--- lists/linked_list.go
+++ lists/linked_list.go
@@ -5,3 +5,21 @@ import "fmt"
 func Hello() {
 	fmt.Println("Hello from linked_list")
 }
+
+type LinkedList[T comparable] struct {
+	Next  *LinkedList[T]
+	Value T
+}
+
+func LinkedListFromSlice[T comparable](s []T) *LinkedList[T] {
+	if len(s) == 0 {
+		return nil
+	}
+
+	l := &LinkedList[T]{nil, s[len(s)-1]}
+	for i := len(s) - 2; i >= 0; i-- {
+		new_head := LinkedList[T]{l, s[i]}
+		l = &new_head
+	}
+	return l
+}
---
+++ lists/linked_list_test.go
@@ -0,0 +1,44 @@
+package lists
+
+import (
+	"fmt"
+	"testing"
+)
+
+var linkedListFromIntSliceTests = [][]int{
+	{1, 2, 3, 4},
+	{1, 1, 1, 1, 1},
+	{},
+	{-42},
+}
+
+func testLinkedListFromSingleSlice[T comparable](t *testing.T, s []T) {
+	errorStart := fmt.Sprintf("LinkedListFromSlice(%v) ", s)
+
+	cur := LinkedListFromSlice[T](s)
+
+	if len(s) == 0 && cur != nil {
+		t.Errorf("%q must return <nil>", errorStart)
+	}
+
+	for i := 0; i < len(s); i++ {
+		if cur == nil {
+			t.Errorf("%q returned a LinkedList with missing element %v", errorStart, i)
+			break
+		}
+		if cur.Value != s[i] {
+			t.Errorf("%q returned a LinkedList with %v (element %v) != s[%v] = %v", errorStart, cur.Value, i, i, s[i])
+		}
+		cur = cur.Next
+	}
+}
+
+func testLinkedListFromSlice[T comparable](t *testing.T, slices [][]T) {
+	for _, s := range slices {
+		testLinkedListFromSingleSlice[T](t, s)
+	}
+}
+
+func TestLinkedListFromSlice(t *testing.T) {
+	testLinkedListFromSlice[int](t, linkedListFromIntSliceTests)
+}
