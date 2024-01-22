c31509e6b09781de21082df124bb0a633b9e5105
Rufat Badal <19675055+rufat-badal@users.noreply.github.com>
2024-01-22 21:25:00 +0100

    Create new min heap

--- main.go
+++ main.go
@@ -7,14 +7,6 @@ import (
 )
 
 func main() {
-	q := queue.NewFIFOQueue[int]()
-	fmt.Println(q)
-	for i := 0; i < 20; i++ {
-		q.Enqueue(i + 42)
-		fmt.Printf("Enqueued %v, queue: %v, length: %v, capacity: %v\n", i+42, q, q.Length, q.Capacity())
-	}
-	for i := 0; i < 15; i++ {
-		popped, _ := q.Dequeue()
-		fmt.Printf("Dequeued %v, queue: %v, capacity: %v\n", popped, q, q.Capacity())
-	}
+	h := queue.NewMinHeap[int]()
+	fmt.Println(h.Length)
 }
--- queues/heap.go
+++ queues/heap.go
@@ -1,6 +1,17 @@
 package queue
 
-type HeapItem[T interface{}] struct {
+type HeapValueType interface{}
+
+type HeapItem[T HeapValueType] struct {
 	Value  T
 	Weight int
+}
+
+type MinHeap[T HeapValueType] struct {
+	data   []HeapItem[T]
+	Length int
+}
+
+func NewMinHeap[T HeapValueType]() MinHeap[T] {
+	return MinHeap[T]{make([]HeapItem[T], 1), 0}
 }
