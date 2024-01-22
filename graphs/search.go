6b430343bd5dce758fdb48370877caf003214865
Rufat Badal <19675055+rufat-badal@users.noreply.github.com>
2024-01-22 16:59:21 +0100

    Implemented bfs

--- graphs/search.go
+++ graphs/search.go
@@ -1,28 +1,41 @@
 package graphs
 
-type NodeState int
+import (
+	"log"
 
-const (
-	Undiscovered NodeState = iota
-	Discovered
-	Processed
+	queue "github.com/rufat-badal/adm/queues"
 )
 
-func bfsFromNode(graph Graph, state *[]NodeState, parent *[]int, node int) {
+func bfsFromNode(graph Graph, discovered []bool, parent []int, start int) {
+	q := queue.NewFIFOQueue[int]()
+	q.Enqueue(start)
+	discovered[start] = true
+
+	for !q.IsEmpty() {
+		x, e := q.Dequeue()
+		if e != nil {
+			log.Fatal(e)
+		}
+		for y := range graph.Edges[x] {
+			if !discovered[y] {
+				parent[y] = x
+				q.Enqueue(y)
+				discovered[y] = true
+			}
+		}
+	}
 }
 
 func BFS(graph Graph) []int {
-	state := make([]NodeState, graph.NumVertices)
+	discovered := make([]bool, graph.NumVertices)
 	parent := make([]int, graph.NumVertices)
 	for i := 0; i < len(parent); i++ {
 		parent[i] = -1
 	}
 	for node := 0; node < graph.NumVertices; node++ {
-		if state[node] != Undiscovered {
-			continue
+		if !discovered[node] {
+			bfsFromNode(graph, discovered, parent, node)
 		}
-		bfsFromNode(graph, &state, &parent, node)
-
 	}
 	return parent
 }
--- main.go
+++ main.go
@@ -1,26 +1,4 @@
 package main
 
-import (
-	"fmt"
-
-	queue "github.com/rufat-badal/adm/queues"
-)
-
 func main() {
-	q := queue.NewFIFOQueue[int]()
-	for i := 0; i < 17; i++ {
-		q.Enqueue(i)
-		fmt.Printf("Enqueued %v, queue: %v, queue capacity: %v\n", i, q, q.Capacity())
-	}
-	for i := 0; i < 13; i++ {
-		x, e := q.Dequeue()
-		if e != nil {
-			break
-		}
-		fmt.Printf("Dequeued %v, queue: %v, queue capacity: %v\n", x, q, q.Capacity())
-	}
-	for i := 17; i < 33; i++ {
-		q.Enqueue(i)
-		fmt.Printf("Enqueued %v, queue: %v, queue capacity: %v\n", i, q, q.Capacity())
-	}
 }
