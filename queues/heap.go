eda90c8701961c3e54c0e773940f0533492c5d0e
Rufat Badal <19675055+rufat-badal@users.noreply.github.com>
2024-01-23 17:03:51 +0100

    child and parent functions for min heap

--- queues/heap.go
+++ queues/heap.go
@@ -15,3 +15,14 @@ type MinHeap[T HeapValueType] struct {
 func NewMinHeap[T HeapValueType]() MinHeap[T] {
 	return MinHeap[T]{make([]HeapItem[T], 1), 0}
 }
+
+func Parent(i int) int {
+	if i == 0 {
+		return -1
+	}
+	return (i - 1) / 2
+}
+
+func FirstChild(i int) int {
+	return 2*i + 1
+}
