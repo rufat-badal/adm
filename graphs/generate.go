faddac750a8e4fad741a2e8d72c68c5091e677f5
Rufat Badal <19675055+rufat-badal@users.noreply.github.com>
2024-01-24 13:06:19 +0100

    Create new random DAG

--- graphs/generate.go
+++ graphs/generate.go
@@ -27,10 +27,14 @@ func (g *Graph) AddEdge(tail, head int) {
 	}
 }
 
-func NewRandomGraph(nvertices int, edgeProbability float64, directed bool) Graph {
+func newEmptyGraph(nvertices int, directed bool) Graph {
 	edges := make([][]int, nvertices)
 	degree := make([]int, nvertices)
-	graph := Graph{edges, degree, directed, nvertices, 0}
+	return Graph{edges, degree, directed, nvertices, 0}
+}
+
+func NewRandomGraph(nvertices int, edgeProbability float64, directed bool) Graph {
+	graph := newEmptyGraph(nvertices, directed)
 	// This algorithm can be improved if edgeProbability is small.
 	for tail := 0; tail < nvertices-1; tail++ {
 		for head := tail + 1; head < nvertices; head++ {
@@ -39,6 +43,23 @@ func NewRandomGraph(nvertices int, edgeProbability float64, directed bool) Graph
 			}
 			if directed && rand.Float64() < edgeProbability {
 				graph.AddEdge(head, tail)
+			}
+		}
+	}
+	return graph
+}
+
+func NewRandomDAG(nvertices int, edgeProbability float64) Graph {
+	// We will create a graph whose vertices have this exact topological sorting:
+	verticesSorted := rand.Perm(nvertices)
+	graph := newEmptyGraph(nvertices, true)
+	for i := 0; i < len(verticesSorted)-1; i++ {
+		// Assure that verticesSorted is the topological sorting of the graph
+		graph.AddEdge(verticesSorted[i], verticesSorted[i+1])
+		// Add (possibly) further edges depending on the edge probability
+		for j := i + 1; j < len(verticesSorted); j++ {
+			if rand.Float64() < edgeProbability {
+				graph.AddEdge(i, j)
 			}
 		}
 	}
--- main.go
+++ main.go
@@ -2,19 +2,11 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 
-	queue "github.com/rufat-badal/adm/queues"
+	"github.com/rufat-badal/adm/graphs"
 )
 
 func main() {
-	h := queue.NewMinHeap[int]()
-	for i := 0; i < 10; i++ {
-		h.Insert(queue.HeapItem[int]{Value: i, Weight: rand.Intn(100)})
-		fmt.Printf("Inserted an item into the heap, data: %v, length: %v, capacity: %v\n", h, h.Length(), h.Capacity())
-	}
-	for i := 0; i < 10; i++ {
-		min, _ := h.ExtractMin()
-		fmt.Printf("Extracted minimum %v from heap, data: %v, length: %v, capacity: %v\n", min, h, h.Length(), h.Capacity())
-	}
+	h := graphs.NewRandomDAG(100, 0.1)
+	fmt.Println(h.NumEdges)
 }
