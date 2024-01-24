ba523a0eaceceb4adbc88fd84a434c74ccf11501
Rufat Badal <19675055+rufat-badal@users.noreply.github.com>
2024-01-24 12:55:15 +0100

    Implemented the min heap

--- main.go
+++ main.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 
 	queue "github.com/rufat-badal/adm/queues"
 )
 
 func main() {
 	h := queue.NewMinHeap[int]()
-	fmt.Println(h.Length)
+	for i := 0; i < 10; i++ {
+		h.Insert(queue.HeapItem[int]{Value: i, Weight: rand.Intn(100)})
+		fmt.Printf("Inserted an item into the heap, data: %v, length: %v, capacity: %v\n", h, h.Length(), h.Capacity())
+	}
+	for i := 0; i < 10; i++ {
+		min, _ := h.ExtractMin()
+		fmt.Printf("Extracted minimum %v from heap, data: %v, length: %v, capacity: %v\n", min, h, h.Length(), h.Capacity())
+	}
 }
--- queues/heap.go
+++ queues/heap.go
@@ -1,5 +1,10 @@
 package queue
 
+import (
+	"errors"
+	"fmt"
+)
+
 type HeapValueType interface{}
 
 type HeapItem[T HeapValueType] struct {
@@ -8,12 +13,11 @@ type HeapItem[T HeapValueType] struct {
 }
 
 type MinHeap[T HeapValueType] struct {
-	data   []HeapItem[T]
-	Length int
+	data []HeapItem[T]
 }
 
 func NewMinHeap[T HeapValueType]() MinHeap[T] {
-	return MinHeap[T]{make([]HeapItem[T], 1), 0}
+	return MinHeap[T]{make([]HeapItem[T], 0)}
 }
 
 func Parent(i int) int {
@@ -25,4 +29,84 @@ func Parent(i int) int {
 
 func FirstChild(i int) int {
 	return 2*i + 1
+}
+
+func (h MinHeap[T]) Length() int {
+	return len(h.data)
+}
+
+func (h MinHeap[T]) IsEmpty() bool {
+	return len(h.data) == 0
+}
+
+func (h *MinHeap[T]) bubbleUp(idx int) {
+	pIdx := Parent(idx)
+	if pIdx == -1 {
+		return
+	}
+	p := h.data[pIdx]
+	c := h.data[idx]
+	if p.Weight <= c.Weight {
+		return
+	}
+	h.data[pIdx], h.data[idx] = c, p
+	h.bubbleUp(pIdx)
+}
+
+func (h *MinHeap[T]) Insert(item HeapItem[T]) {
+	h.data = append(h.data, item)
+	h.bubbleUp(len(h.data) - 1)
+}
+
+func (h MinHeap[T]) Capacity() int {
+	return cap(h.data)
+}
+
+func (h MinHeap[T]) String() string {
+	stringData := make([]string, len(h.data))
+	for i := 0; i < len(h.data); i++ {
+		stringData[i] = fmt.Sprintf("%v", h.data[i])
+	}
+	return fmt.Sprintf("%v", stringData)
+}
+
+func (h *MinHeap[T]) bubbleDown(p int) {
+	min := p
+	c := FirstChild(p)
+	// Find index of the node of minimal weight in the family of the node at p
+	for i := 0; i < 2; i++ {
+		if (c + i) >= len(h.data) {
+			break
+		}
+		if h.data[c+i].Weight < h.data[min].Weight {
+			min = c + i
+		}
+	}
+
+	if min != p {
+		h.data[p], h.data[min] = h.data[min], h.data[p]
+		h.bubbleDown(min)
+	}
+}
+
+func (h *MinHeap[T]) decreaseCapacity() {
+	if len(h.data) < cap(h.data)/4 {
+		newData := make([]HeapItem[T], len(h.data), cap(h.data)/2)
+		copy(newData, h.data)
+		h.data = newData
+	}
+}
+
+func (h *MinHeap[T]) ExtractMin() (HeapItem[T], error) {
+	if h.IsEmpty() {
+		return *new(HeapItem[T]), errors.New("cannot extract minimum from an empty heap")
+	}
+	min := h.data[0]
+	h.data[0] = h.data[len(h.data)-1]
+	h.data = h.data[:len(h.data)-1]
+	if len(h.data) > 0 {
+		h.bubbleDown(0)
+	}
+	h.decreaseCapacity()
+	return min, nil
 }
