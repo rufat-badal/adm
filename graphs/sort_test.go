c7bde83328ffd049fb49af01ed9ced59afff289d
Rufat Badal <19675055+rufat-badal@users.noreply.github.com>
2024-01-25 19:32:50 +0100

    tested topological sort

---
+++ graphs/sort_test.go
@@ -0,0 +1,28 @@
+package graphs
+
+import (
+	"math/rand"
+	"slices"
+	"testing"
+)
+
+func TestTopologicalSort(t *testing.T) {
+	r := rand.New(rand.NewSource(42))
+	nvertices := 1000
+	sortedWant := r.Perm(nvertices)
+	g := NewRandomDAG(sortedWant, 0.1)
+	sortedGot, e := TopologicalSort(g)
+	if e != nil {
+		t.Error(e)
+	} else if !slices.Equal(sortedWant, sortedGot) {
+		t.Errorf("TopologicalSort returned %v, wanted %v", sortedGot, sortedWant)
+	}
+	g.addDirectedEdge(sortedWant[nvertices-1], sortedWant[0])
+	sortedWant = make([]int, 0)
+	sortedGot, e = TopologicalSort(g)
+	if e == nil {
+		t.Errorf("Topological did not return an error")
+	} else if !slices.Equal(sortedGot, sortedWant) {
+		t.Errorf("TopologicalSort returned %v, wanted %v", sortedGot, sortedWant)
+	}
+}
