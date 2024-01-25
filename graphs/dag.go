6750c48d1bdc3c416af3d6571cdc601e512bc50a
Rufat Badal <19675055+rufat-badal@users.noreply.github.com>
2024-01-25 18:07:08 +0100

    first version of dfsSort

--- graphs/dag.go
+++ graphs/dag.go
@@ -1,6 +1,10 @@
 package graphs
 
-import queue "github.com/rufat-badal/adm/queues"
+import (
+	"errors"
+
+	queue "github.com/rufat-badal/adm/queues"
+)
 
 type NodeState int
 
@@ -10,21 +14,33 @@ const (
 	Processed
 )
 
-func dfsSort(g Graph, x int, states []NodeState, parent []int, cycleDetected *bool, s *queue.Stack[int]) {
+func dfsSort(g Graph, x int, states []NodeState, parent []int, s *queue.Stack[int]) bool {
 	states[x] = Discovered
 	for y := range g.Edges[x] {
 		if states[y] == Undiscovered {
 			parent[y] = x
-			dfsSort(g, y, states, parent, cycleDetected, s)
-		} else if (!g.Directed && )
+			dfsSort(g, y, states, parent, s)
+		} else if states[y] != Processed {
+			return true
+		}
 	}
+	return false
 }
 
-func TopologicalSort(g Graph) []int {
+func TopologicalSort(g Graph) ([]int, error) {
 	s := queue.NewStack[int]()
 	states := make([]NodeState, g.NumVertices)
 	parent := make([]int, g.NumVertices)
 	for i := 0; i < len(parent); i++ {
 		parent[i] = -1
+	}
+	cyclic := false
+	for i := 0; i < g.NumVertices; i++ {
+		if states[i] == Undiscovered {
+			cyclic = dfsSort(g, i, states, parent, &s)
+		}
+		if cyclic {
+			return *new([]int), errors.New("cycle detected, cannot sort cyclic graph")
+		}
 	}
 }
