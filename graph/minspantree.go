875200724f030b108788dbc89aadd5d3c88a8f60
Rufat Badal <19675055+rufat-badal@users.noreply.github.com>
2024-02-14 17:34:25 +0100

    Finished implementation of Prim's algorithm

--- graph/minspantree.go
+++ graph/minspantree.go
@@ -1,24 +1,60 @@
 package graph
 
-import "github.com/rufat-badal/adm/queue"
+import (
+	"errors"
+	"fmt"
+
+	"github.com/rufat-badal/adm/queue"
+)
 
 const maxUInt = ^uint(0)
 const MAXINT = int(maxUInt >> 1)
 
 type MinSpanTreeResult struct {
-	weight int
-	parent []int
+	Weight int
+	Parent []int
 }
 
-func MinSpanTreePrim(g Graph) MinSpanTreeResult {
-	weight := 0
-	parent := make([]int, g.NumVertices)
-	for i := 0; i < len(parent); i++ {
-		parent[i] = -1
+func MinSpanTreePrim(g Graph, start int) (MinSpanTreeResult, error) {
+	if start < 0 || start >= g.NumVertices {
+		return *new(MinSpanTreeResult), fmt.Errorf("%v is not an admissible grid vertex (only 0, ..., %v allowed)", start, g.NumVertices-1)
 	}
+	res := MinSpanTreeResult{0, make([]int, g.NumVertices)}
+	for i := 0; i < len(res.Parent); i++ {
+		res.Parent[i] = -1
+	}
+	// heap item weight is the minimimal distance to tree vertices
+	// heap item value is node index
 	nodes := make([]queue.HeapItem[int], g.NumVertices)
 	for i := 0; i < len(nodes); i++ {
+		nodes[i].Value = i
 		nodes[i].Weight = MAXINT
 	}
-	nodesHeap := queue.NewMinHeap[primNode](nodes)
+	nodesHeap := queue.NewMinHeap[int](nodes)
+	inTree := make([]bool, g.NumVertices)
+	nodesHeap.DecreaseWeight(start, 0)
+
+	var v, dist int
+	for n, e := nodesHeap.ExtractMin(); e != nil; n, e = nodesHeap.ExtractMin() {
+		if inTree[n.Value] {
+			continue
+		}
+		if n.Weight == MAXINT {
+			return res, errors.New("graph was not connected, the returned minimum spanning tree does not contain all nodes")
+		}
+		v, dist = n.Value, n.Weight
+		inTree[v] = true
+		res.Weight += dist
+		for _, edge := range g.Edges[v] {
+			if inTree[edge.Head] {
+				continue
+			}
+			oldDist := nodesHeap.DecreaseWeight(edge.Head, edge.Weight)
+			if oldDist > edge.Weight {
+				res.Parent[edge.Head] = v
+			}
+		}
+	}
+
+	return res, nil
 }
