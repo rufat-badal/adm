73eb971b81d67cf53e1d342aa904f6a783712a0f
Rufat Badal <19675055+rufat-badal@users.noreply.github.com>
2024-02-26 15:34:46 +0100

    Created a flake for the project

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Nix flake for adm";
+
+  inputs = {
+      nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+  in
+  {
+    devShells.${system}.default =
+      pkgs.mkShell
+        {
+          buildInputs = with pkgs; [
+            go
+            gopls
+          ];
+          # shellHook = ''
+          # '';
+        }; 
+  };
+}
