f909088608f92623de20e5fe9030589821657027
Rufat Badal <19675055+rufat-badal@users.noreply.github.com>
2024-03-02 21:41:16 +0100

    Generate single random flow path

--- graph/maxflow_test.go
+++ graph/maxflow_test.go
@@ -54,9 +54,64 @@ func newRandomGeneralGraph(
 	return g
 }
 
+func (g *Graph) addPathEdges(tail int, head int, rcap func() int, dprob float64, r *rand.Rand) int {
+	randc := rcap()
+	totalc := randc
+	fmt.Printf("adding edge from %v to %v with capacity %v\n", tail, head, randc)
+	g.AddEdge(tail, head, randc)
+	for r.Float64() < dprob {
+		fmt.Printf("adding edge from %v to %v\n", tail, head)
+		randc = rcap()
+		totalc += randc
+		g.AddEdge(tail, head, randc)
+	}
+	return totalc
+}
+
+func (g *Graph) addPath(
+	nedges int,
+	source int,
+	sink int,
+	rcap func() int,
+	dprob float64,
+	r *rand.Rand,
+) int {
+	tail := source
+	var head, pathc int
+	if nedges == 1 {
+		head = sink
+		pathc = g.addPathEdges(tail, head, rcap, dprob, r)
+		return pathc
+	}
+
+	nextv := g.NumVertices
+	g.Edges = append(g.Edges, make([][]Edge, nedges-1)...)
+	g.Degree = append(g.Degree, make([]int, nedges-1)...)
+	g.NumVertices += nedges - 1
+	head = nextv
+	nextv++
+	pathc = g.addPathEdges(tail, head, rcap, dprob, r)
+
+	var curc int
+	for i := 0; i < nedges-1; i++ {
+		tail = head
+		if i < nedges-2 {
+			head = nextv
+			nextv++
+		} else {
+			head = sink
+		}
+		curc = g.addPathEdges(tail, head, rcap, dprob, r)
+		if curc < pathc {
+			pathc = curc
+		}
+	}
+
+	return pathc
+}
+
 func TestMaxFlow(t *testing.T) {
 	r := rand.New(rand.NewSource(RAND_SEED))
-	g := newRandomGeneralGraph(100, true, 0.1, 0.01, 100, r)
-	fmt.Println(g.MaxFlow(0, 42))
-	fmt.Println(g.MaxFlow(1, 69))
+	g := newEmptyGraph(2, true)
+	fmt.Println(g.addPath(10, 0, 1, randCapacity(10, 20, r), 0.01, r))
 }
