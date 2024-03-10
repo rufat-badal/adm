616e21d9eb03f0cf100aaaffcc2471fa09a8d202
Rufat Badal <19675055+rufat-badal@users.noreply.github.com>
2024-03-10 11:24:08 +0100

    Use direnv

--- flake.nix
+++ flake.nix
@@ -2,21 +2,24 @@
   description = "Nix flake for adm";
 
   inputs = {
-      nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-      flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
-  flake-utils.lib.eachDefaultSystem (system:
-  let
-      pkgs = nixpkgs.legacyPackages.${system};
-  in
-  {
-    devShell = pkgs.mkShell {
-      buildInputs = with pkgs; [
-        go
-        gopls
-      ];
-    };
-  });
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            go
+            gopls
+            delve
+            go-tools
+            rnix-lsp
+          ];
+        };
+      });
 }
